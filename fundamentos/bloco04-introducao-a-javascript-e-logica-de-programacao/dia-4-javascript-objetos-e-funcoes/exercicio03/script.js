f4a20f03bbbbefd8234de7118fde1335ac83e4bf
Andrew Lima <andrewmll94@gmail.com>
2022-08-23 15:55:13 -0300

    Conclusão dos exercícios 1 ao 3

---
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio03/script.js
@@ -0,0 +1,12 @@
+//Faça um for/in mostrando todas as chaves do objeto
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
+
+for (const key in info) {
+  console.log(key);
+}
