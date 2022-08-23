cd25de784103c632d73dff7e06470ddae831ec19
Andrew Lima <andrewmll94@gmail.com>
2022-08-23 18:24:54 -0300

    Conclusão do exercício 6

---
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio06/script.js
@@ -0,0 +1,16 @@
+//Acesse as chaves nome, sobrenome e título que está dentro da chave livrosFavoritos e faça um console.log
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+console.log('O livro favorido de ' + leitor.nome + ' ' + leitor.sobrenome + ' ' + 'se chama ' + leitor.livrosFavoritos[0].titulo);
