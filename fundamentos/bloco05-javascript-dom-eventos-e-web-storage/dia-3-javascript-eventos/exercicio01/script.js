487ba037aa9179d61793eb2a1eec8d0fead45e6f
Andrew Lima <andrewmll94@gmail.com>
2022-08-29 20:58:17 -0300

    Conclusão 2° exercício

--- fundamentos/bloco05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicio01/script.js
+++ fundamentos/bloco05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicio01/script.js
@@ -20,7 +20,7 @@ function criadorDeFuncoes (tag, elementoPai) {
   elementoPai.appendChild(variavel);
 };
 
-/* Criação de função para criar dinamicamente cada dia no calendário e acrescentar dentro de um tag <ul>, bem como criação de algumas classes, respeitando determinados parâmetros previamente estipulados pela Trybe */
+/* #1 Criação de função para criar dinamicamente cada dia no calendário e acrescentar dentro de um tag <ul>, bem como criação de algumas classes, respeitando determinados parâmetros previamente estipulados pela Trybe */
 
 let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 const dia = document.getElementById('days');
@@ -32,16 +32,24 @@ function criaLi() {
     dia3.innerHTML = dia2;
     dia.appendChild(dia3);
     if (dia2 === 24 || dia2 === 25 || dia2 === 31) {
-      dia3.className = 'day holiday'
+      dia3.className = 'day holiday';
       dia.appendChild(dia3)
     } else if (dia2 === 4 || dia2 === 11 || dia2 === 18 || dia2 === 25) {
-      dia3.className = 'day friday'
-      dia.appendChild(dia3)
+      dia3.className = 'day friday';
+      dia.appendChild(dia3);
     } else {
-      dia3.className = 'day'
-      dia.appendChild(dia3)
+      dia3.className = 'day';
+      dia.appendChild(dia3);
     }
   }
 }
 
 criaLi()
+
+/* #2 Criando um botão dinamicamente */
+
+const divBotao = document.querySelector('.buttons-container');
+criadorDeFuncoes('button', divBotao);
+const botao = document.querySelector('button');
+botao.setAttribute('id', 'btn-holiday');
+botao.innerText = 'Feriados';
