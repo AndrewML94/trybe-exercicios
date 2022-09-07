12775365c433e140ebfee9c1c320bc8df9aa667c
Andrew Lima <andrewmll94@gmail.com>
2022-09-06 21:48:48 -0300

    Conclusão dos exercícios bônus do dia

--- fundamentos/bloco06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio01/script.js
+++ fundamentos/bloco06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio01/script.js
@@ -1,6 +1,7 @@
-const buttonSubmit = document.getElementById('submit');
+const buttonSubmit = document.getElementById('btnSubmit');
 const divButton = document.getElementById('button');
 const form = document.getElementById('my-form');
+const option2 = document.getElementById('option02');
 
 buttonSubmit.addEventListener('click', function(event) {
   event.preventDefault();
@@ -12,11 +13,17 @@ function createButtonClear() {
   buttonClear.innerText = 'Limpar';
   divButton.appendChild(buttonClear);
 }
-
 createButtonClear()
 
 const buttonClear = document.getElementById('clear');
 buttonClear.addEventListener('click', function(event) {
   event.preventDefault();
   event.target.form.reset();
+})
+
+buttonSubmit.addEventListener('click', function() {
+  if (option2.checked) {
+    form.submit()
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+  }
 })
