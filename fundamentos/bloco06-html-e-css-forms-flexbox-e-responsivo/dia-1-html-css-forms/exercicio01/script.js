4c8ecc8e440f3c04ebc87cfdb8a4ad64a7600c13
Andrew Lima <andrewmll94@gmail.com>
2022-09-06 21:06:56 -0300

    Criação do botão clear e preventDefault do botão submit

--- fundamentos/bloco06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio01/script.js
+++ fundamentos/bloco06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio01/script.js
@@ -1,3 +1,22 @@
-const buttonSubmit = document.getElementById(submit).type.submit;
-const buttonClear = document.getElementById(clear).type.reset;
+const buttonSubmit = document.getElementById('submit');
+const divButton = document.getElementById('button');
+const form = document.getElementById('my-form');
 
+buttonSubmit.addEventListener('click', function(event) {
+  event.preventDefault();
+})
+
+function createButtonClear() {
+  const buttonClear = document.createElement('button');
+  buttonClear.setAttribute('id', 'clear');
+  buttonClear.innerText = 'Limpar';
+  divButton.appendChild(buttonClear);
+}
+
+createButtonClear()
+
+const buttonClear = document.getElementById('clear');
+buttonClear.addEventListener('click', function(event) {
+  event.preventDefault();
+  event.target.form.reset();
+})
