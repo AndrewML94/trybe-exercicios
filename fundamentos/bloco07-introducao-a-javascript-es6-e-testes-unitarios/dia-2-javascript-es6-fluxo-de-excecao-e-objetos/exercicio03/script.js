3f148b1ccb8b0096fbc11fddd455e448565f4427
Andrew <andrewmll94@gmail.com>
2022-09-18 20:46:54 -0300

    Conclusão exercício 3.6

--- fundamentos/bloco07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exercicio03/script.js
+++ fundamentos/bloco07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exercicio03/script.js
@@ -44,4 +44,9 @@ valores(lesson3);
 //Criar uma função que agrupará todas as aulas em um novo objeto//
 
 const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
-console.log(allLessons)
+
+//Criar uma função para retornar a soma de todos os estudantes em todas as aulas//
+
+const totalAlunos = () => `O total de alunos nas aulas é de: ${lesson1.numeroEstudantes + lesson2.numeroEstudantes + lesson3.numeroEstudantes}`;
+console.log(totalAlunos())
+
