bcac883fef28329aa5979661b77594971a88e44f
Andrew <andrewmll94@gmail.com>
2022-09-28 10:21:58 -0300

    Conclusão do exercício 3.2

--- fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia-5-spread-operator-paramentro-rest-destructuring-e-mais/exercicio03/script.js
+++ fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia-5-spread-operator-paramentro-rest-destructuring-e-mais/exercicio03/script.js
@@ -2,10 +2,17 @@
 
 const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
 
-saudacoes[1](saudacoes[0]); // Olá
-
 // Produza o mesmo resultado acima, porém utilizando array destructuring
 
 const [saudacoes0] = saudacoes;
 
-console.log(saudacoes0);
+// #2 A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis. 
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+const all = [comida, animal, bebida];
+const [animal1, bebida1, comida1] = all;
