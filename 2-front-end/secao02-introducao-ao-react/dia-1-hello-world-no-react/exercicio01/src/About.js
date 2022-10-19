2a2da619335ed16ae415d9b743d4a7b3fadf9e16
Andrew <andrewmll94@gmail.com>
2022-10-19 19:06:39 -0300

    Exercício 1

---
+++ 2-front-end/secao02-introducao-ao-react/dia-1-hello-world-no-react/exercicio01/src/About.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class About extends React.Component {
+  render() {
+    return (
+      <body>
+        <header>
+          <h1>Andrew Lima</h1>
+          <p>Um administrador que está aprendendo sobre programação para mudar de vida!!</p>
+        </header>
+      <main>
+        <h2>Minhas habilidades</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>Comunicação</li>
+          <li>Trabalho em equipe</li>
+          <li>Negociação</li>
+          <li>Inteligência emocional</li>
+        </ul>
+      </main>
+      </body>
+    )
+  }
+}
+
+export default About;
