540fbe96e819398cfe4c7dd9af9336f2d114b301
Andrew <andrewmll94@gmail.com>
2022-10-20 12:31:15 -0300

    Comentário no Footer com o enunciado do exercício

--- 2-front-end/secao02-introducao-ao-react/dia-1-hello-world-no-react/exercicio02/src/Footer.js
+++ 2-front-end/secao02-introducao-ao-react/dia-1-hello-world-no-react/exercicio02/src/Footer.js
@@ -1,3 +1,8 @@
+// #3 Crie um arquivo chamado Footer.js que representará o componente Footer:
+// - Este arquivo precisa ser criado na pasta src;
+// - Este componente deve renderizar uma tag footer e, dentro dela, renderizar um h3 com o texto 'E isso é só o começo...';
+// - A tag footer precisa do atributo data-testid="footer".
+
 import React from 'react';
 
 class Footer extends React.Component {
