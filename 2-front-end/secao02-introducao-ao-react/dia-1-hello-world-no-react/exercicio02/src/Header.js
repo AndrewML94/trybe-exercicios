b0c083f4e5bc09f8a45bebe67e9297dd3ae1706d
Andrew <andrewmll94@gmail.com>
2022-10-20 12:32:37 -0300

    Comentário no header com o enunciado do exercício

--- 2-front-end/secao02-introducao-ao-react/dia-1-hello-world-no-react/exercicio02/src/Header.js
+++ 2-front-end/secao02-introducao-ao-react/dia-1-hello-world-no-react/exercicio02/src/Header.js
@@ -1,3 +1,7 @@
+// #1 Crie um arquivo chamado Header.js que representará o componente Header:
+// - Este arquivo precisa ser criado na pasta src;
+// - Este componente deve renderizar uma tag h1 com o texto 'Conteúdos de Frontend'.
+
 import React from 'react';
 
 class Header extends React.Component {
