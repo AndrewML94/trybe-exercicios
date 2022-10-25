5c1f9469c13f94a5cdfa24267311650a8c482dbe
Andrew <andrewmll94@gmail.com>
2022-10-25 11:34:03 -0300

    Criação do app.js

--- 2-front-end/secao03-componentes-com-estado-eventos-e-formularios-com-react/exercicio01/src/App.js
+++ 2-front-end/secao03-componentes-com-estado-eventos-e-formularios-com-react/exercicio01/src/App.js
@@ -1,21 +1,14 @@
+import React from 'react';
+
 import './App.css';
+import Pokedex from './components/Pokedex';
+
+import pokemonList from './data';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pokedex pokemonList={ pokemonList } />
     </div>
   );
 }
