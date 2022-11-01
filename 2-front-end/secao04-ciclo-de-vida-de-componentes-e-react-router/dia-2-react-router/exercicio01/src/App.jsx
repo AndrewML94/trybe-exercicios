15644d6b21c6e88a5b13a70f46a8f502c11cde0e
Andrew <andrewmll94@gmail.com>
2022-11-01 19:07:02 -0300

    Conclusão do exercício 02 :rocket:

--- 2-front-end/secao04-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/exercicio01/src/App.jsx
+++ 2-front-end/secao04-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/exercicio01/src/App.jsx
@@ -1,11 +1,12 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        
+        <Route exact path="/" component={ Home } />
       </BrowserRouter>
     </div>
   );
