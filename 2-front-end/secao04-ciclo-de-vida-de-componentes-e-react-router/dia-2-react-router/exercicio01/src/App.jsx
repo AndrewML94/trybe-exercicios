739365d89af1a7346b221683c8134ae406da5ef4
Andrew <andrewmll94@gmail.com>
2022-11-02 21:57:01 -0300

    Conclusão do exercícios 6, 7 e 8

--- 2-front-end/secao04-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/exercicio01/src/App.jsx
+++ 2-front-end/secao04-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/exercicio01/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route exact path="/about" component={ About } />
-          <Route exact path="/users" component={ Users } />
+          <Route exact path="/users" render={(greetingsMessage) => <Users {...greetingsMessage} greetingsMessage='Good Morning'/> } />
         </Switch>
       </BrowserRouter>
     </div>
--- 2-front-end/secao04-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/exercicio01/src/pages/Users.jsx
+++ 2-front-end/secao04-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/exercicio01/src/pages/Users.jsx
@@ -3,10 +3,12 @@ import Navbar from '../components/Navbar';
 
 class Users extends Component {
   render() {
+    const { greetingsMessage } = this.props;
     return (
       <div>
         <Navbar />
         <h3>Você está na página Usuários</h3>
+        <p> { greetingsMessage }, My awesome Users component </p>
       </div>
     )
   }
