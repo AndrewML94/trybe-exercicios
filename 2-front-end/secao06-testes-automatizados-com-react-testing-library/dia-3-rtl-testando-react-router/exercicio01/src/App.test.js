2caa13d68ecf2907bff2fc59b57d2875cc24b786
Andrew Lima <andrewmll94@gmail.com>
2022-11-21 15:16:43 -0300

    Criação do teste para um caminho não existente (Teste de Rota, Link e H1)

--- 2-front-end/secao06-testes-automatizados-com-react-testing-library/dia-3-rtl-testando-react-router/exercicio01/src/App.test.js
+++ 2-front-end/secao06-testes-automatizados-com-react-testing-library/dia-3-rtl-testando-react-router/exercicio01/src/App.test.js
@@ -1,28 +1,43 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from './App';
 
-it('Deve renderizar o componente App.', () => {
-  renderWithRouter(<App />);
 
-  const homeTitle = screen.getByRole('heading', {
-    name: 'Você está na página Início',
+describe('Teste da aplicação toda.', () => {
+  it('Deve renderizar o componente App.', () => {
+    renderWithRouter(<App />);
+  
+    const homeTitle = screen.getByRole('heading', {
+      name: 'Você está na página Início',
+    });
+    expect(homeTitle).toBeInTheDocument();
   });
-  expect(homeTitle).toBeInTheDocument();
-});
+  
+  it('Deve rendenizar o componente sobre', () => {
+    const { history } = renderWithRouter(<App />);
+  
+    const aboutLink = screen.getByRole('link', { name: 'Sobre' });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+  
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  
+    const aboutTitle = screen.getByRole('heading', { name: 'Você está na página Sobre' })
+    expect(aboutTitle).toBeInTheDocument();
+  })
 
-it('Deve rendenizar o componente sobre', () => {
-  const { history } = renderWithRouter(<App />);
+  it('Deve testar um caminho não existente e a rendenização do "Not Found".', () => {
+    const { history } = renderWithRouter(<App />);
 
-  const aboutLink = screen.getByRole('link', { name: 'Sobre' });
-  expect(aboutLink).toBeInTheDocument();
-  userEvent.click(aboutLink);
+    act(() => {
+      history.push('/pagina/que-nao-existe/')
+    })
 
-  const { pathname } = history.location;
-  expect(pathname).toBe('/about');
-
-  const aboutTitle = screen.getByRole('heading', { name: 'Você está na página Sobre' })
-  expect(aboutTitle).toBeInTheDocument();
+    const notFoundTitle = screen.getByRole('heading', { name: 'Página não encontrada' });
+    expect(notFoundTitle).toBeInTheDocument();
+  })
+  
 })
