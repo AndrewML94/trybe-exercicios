da371c538584776571e45bb0fb8f489b636819e8
Andrew Lima <andrewmll94@gmail.com>
2023-03-07 12:18:27 -0300

    Conclusão dos exercício 3 e 4

--- 3-back-end/secao04-introducao-ao-desenvolvimento-web-com-node/dia-1-node-runtime-assincrono/exercicio01/src/bmi.js
+++ 3-back-end/secao04-introducao-ao-desenvolvimento-web-com-node/dia-1-node-runtime-assincrono/exercicio01/src/bmi.js
@@ -1,6 +1,18 @@
+const { questionFloat } = require('readline-sync');
+
+const questionWeight = () => {
+  const weight = questionFloat('What is your weight? (KG) ');
+  return weight;
+};
+
+const questionHeight = () => {
+  const height = questionFloat('What is your height? (MT) ');
+  return height;
+};
+
 const imc = (weight, height) => {
   const result = weight / (height * height);
   return `Seu IMC é de: ${result.toFixed(2)}`;
 }
 
-console.log(imc(80, 1.80));
+console.log(imc(questionHeight(), questionWeight()));
