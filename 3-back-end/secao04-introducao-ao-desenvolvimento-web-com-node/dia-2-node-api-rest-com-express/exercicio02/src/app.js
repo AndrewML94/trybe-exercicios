cde3806ccff8cbe9b039d3674f5f8bde550194bb
Andrew Lima <andrewmll94@gmail.com>
2023-03-07 23:02:34 -0300

    Criação de um endpoint get com a rota /movies/:id e mostrar o JSON do filme pelo id do endpoint

--- 3-back-end/secao04-introducao-ao-desenvolvimento-web-com-node/dia-2-node-api-rest-com-express/exercicio02/src/app.js
+++ 3-back-end/secao04-introducao-ao-desenvolvimento-web-com-node/dia-2-node-api-rest-com-express/exercicio02/src/app.js
@@ -8,13 +8,22 @@ app.use(express.json());
 
 const readMovies = async () => {
   try {
-    const data = await readFile('./src/movies.json', 'utf-8');
-    console.log(data);
+    const data = await readFile('./src/movies.json');
+    return JSON.parse(data);
   } catch (error) {
     console.error(`Erro ao ler o arquivo ${error.path}`);
   }
 };
 
-readMovies();
+app.get('/movies/:id', async (req, res) => {
+  try {
+    const idParams = req.params;
+    const movies = await readMovies();
+    const filterMovieId = movies.find(({ id }) => id === Number(idParams.id));
+    res.status(200).json(filterMovieId);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
 
 module.exports = app;
