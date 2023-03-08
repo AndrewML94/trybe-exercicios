d847e602392a02689d31cf8c2003d7fc1e4c0272
Andrew Lima <andrewmll94@gmail.com>
2023-03-07 22:25:42 -0300

    Criação de função para leitura do JSON com fs

--- 3-back-end/secao04-introducao-ao-desenvolvimento-web-com-node/dia-2-node-api-rest-com-express/exercicio02/src/app.js
+++ 3-back-end/secao04-introducao-ao-desenvolvimento-web-com-node/dia-2-node-api-rest-com-express/exercicio02/src/app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 
+const { readFile } = require('fs').promises;
+
 const app = express();
 
 app.use(express.json());
+
+const readMovies = async () => {
+  try {
+    const data = await readFile('./src/movies.json', 'utf-8');
+    console.log(data);
+  } catch (error) {
+    console.error(`Erro ao ler o arquivo ${error.path}`);
+  }
+};
+
+readMovies();
 
 module.exports = app;
