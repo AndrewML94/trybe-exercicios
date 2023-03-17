73c266cded3b7cc37ffe579985b7f731f9fa0ebb
Andrew Lima <andrewmll94@gmail.com>
2023-03-17 14:51:01 -0300

    Refatorando o endpoint /passengers/:passengerId/request/travel e /drivers/open/travels bem como criando testes TDD

--- 3-back-end/secao05-arquitetura-de-software-model-service-e-controller/dias-1-camada-model-2-camada-service-3-camada-controller/msc-architecture-trybecar/tests/unit/models/travel.model.test.js
+++ 3-back-end/secao05-arquitetura-de-software-model-service-e-controller/dias-1-camada-model-2-camada-service-3-camada-controller/msc-architecture-trybecar/tests/unit/models/travel.model.test.js
@@ -3,7 +3,7 @@ const sinon = require('sinon');
 
 const connection = require('../../../src/models/connection');
 const { travelModel } = require('../../../src/models');
-const { newTravel } = require('./mocks/travel.model.mock');
+const { travelsFromDB, travels, newTravel } = require('./mocks/travel.model.mock');
 
 describe('Testes de unidade do model de viagens', function () {
   it('Realizando uma operação de INSERT com o model travel', async function () {
@@ -12,6 +12,22 @@ describe('Testes de unidade do model de viagens', function () {
     const result = await travelModel.insert(newTravel);
 
     expect(result).to.equal(42);
+  });
+
+  it('Recuperando uma travel a partir do seu id', async function () {
+    sinon.stub(connection, 'execute').resolves([[travelsFromDB[0]]]);
+
+    const result = await travelModel.findById(1);
+
+    expect(result).to.deep.equal(travels[0]);
+  });
+
+  it('Recuperando as travels a partir do seu travel_status_id', async function () {
+    sinon.stub(connection, 'execute').resolves([travelsFromDB]);
+
+    const result = await travelModel.findByTravelStatusId(1);
+
+    expect(result).to.deep.equal(travels);
   });
 
   afterEach(function () {
