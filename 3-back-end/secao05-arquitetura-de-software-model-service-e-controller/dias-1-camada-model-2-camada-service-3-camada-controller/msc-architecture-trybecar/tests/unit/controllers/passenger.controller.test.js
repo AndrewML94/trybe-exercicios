8f5715cf11600afacd20c2b04c5ed1dbd4c80059
Andrew Lima <andrewmll94@gmail.com>
2023-03-20 16:05:22 -0300

    feat: Criação da função getPassenger e seus testes

--- 3-back-end/secao05-arquitetura-de-software-model-service-e-controller/dias-1-camada-model-2-camada-service-3-camada-controller/msc-architecture-trybecar/tests/unit/controllers/passenger.controller.test.js
+++ 3-back-end/secao05-arquitetura-de-software-model-service-e-controller/dias-1-camada-model-2-camada-service-3-camada-controller/msc-architecture-trybecar/tests/unit/controllers/passenger.controller.test.js
@@ -6,10 +6,10 @@ const { expect } = chai;
 chai.use(sinonChai);
 
 const { passengerService } = require('../../../src/services');
-const { passencerController } = require('../../../src/controllers');
-const { passengerListMock } = require('./mocks/passenger.controller.mock');
+const { passengerController } = require('../../../src/controllers');
+const { passengerListMock, passengerMock, newPassengerMock } = require('./mocks/passenger.controller.mock');
 
-describe('Teste de unidade do passengerController', function () {
+describe('Testes de unidade do passengerController', function () {
   describe('Listando as pessoa passageiras', function () {
     it('Deve retornar o status 200 e a lista', async function () {
       const res = {};
@@ -19,10 +19,66 @@ describe('Teste de unidade do passengerController', function () {
       res.json = sinon.stub().returns();
       sinon.stub(passengerService, 'findAll').resolves({ type: null, message: passengerListMock });
 
-      await passencerController.listPassengers(req, res);
+      await passengerController.listPassengers(req, res);
 
       expect(res.status).to.have.been.calledWith(200);
       expect(res.json).to.have.been.calledWith(passengerListMock);
+    });
+  });
+
+  describe('Buscando uma pessoa passageira', function () {
+    it('deve responder com 200 e os dados do banco quando existir', async function () {
+      const res = {};
+      const req = {
+        params: { id: 1 },
+      };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(passengerService, 'findById')
+        .resolves({ type: null, message: newPassengerMock });
+
+      await passengerController.getPassenger(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(newPassengerMock);
+    });
+
+    it('ao passar um id inválido deve retornar um erro', async function () {
+      const res = {};
+      const req = {
+        params: { id: 'abc' },
+      };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(passengerService, 'findById')
+        .resolves({ type: 'INVALID_VALUE', message: '"id" must be a number' });
+
+      await passengerController.getPassenger(req, res);
+
+      expect(res.status).to.have.been.calledWith(422); 
+      expect(res.json).to.have.been.calledWith('"id" must be a number');
+    });
+
+    it('ao passar um id que não existe no banco deve retornar um erro', async function () {
+      const res = {};
+      const req = {
+        params: { id: 9999 },
+      };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(passengerService, 'findById')
+        .resolves({ type: 'PASSENGER_NOT_FOUND', message: 'Passenger not found' });
+
+      await passengerController.getPassenger(req, res);
+
+      expect(res.status).to.have.been.calledWith(404); 
+      expect(res.json).to.have.been.calledWith('Passenger not found');
     });
   });
 
